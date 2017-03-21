e074d6356e7a19e5c8e39fa7d9cc0f7a392f9e21
Anatoliy Klots and Peter Karellas <peter@>
2017-03-21 15:06:37 -0700

    finish

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $("#js").click(function() {
+    $("h2#def").toggle();
+  });
+});
+
+
+
+
+
+
+
+  // $("p").click(function() {
+  //   $("img").show();
+
+
+  // });
+  // $(".clickable").click(function() {
+  //   $(".panel-title").toggle();
+  //   $(".hidden").toggle();
+
+  //
+  // $(".click").click(function() {
+  //   $(".showing").toggle();
+  // //   $(".hidden").toggle();
+  // });
+
+// });
